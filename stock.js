f551388ac81f3ad72700eb6ff70514cf818ffd57
philmarcracken <chrismaley@internode.on.net>
2021-06-05 15:13:45 +0800

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,13 @@
+//Out of stock
+
+//GF spin and riccotta rolls
+$("#ComponentGroupItem_35_1188_2").css("color", "magenta");
+
+//GF meat pies
+$("#ComponentGroupItem_35_1288_2").css("color", "magenta");
+
+//Bananas
+//$("#ComponentGroupItem_49_908_2").css("color", "magenta");
+
+//Choc Milk
+$("#ComponentGroupItem_47_1230_2").css("color", "magenta");
