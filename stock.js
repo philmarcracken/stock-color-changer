ac993b7d755d356e0205286450f614edfb63814b
philmarcracken <chrismaley@internode.on.net>
2021-08-09 10:27:28 +0800

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,5 +1,8 @@
+//Out of stock
+
 //GF spin and riccotta rolls
 $("#ComponentGroupItem_35_1188_2").css("color", "magenta");
+
 
 //GF meat pies
 $("#ComponentGroupItem_35_1288_2").css("color", "magenta");
@@ -11,10 +14,13 @@ $("#ComponentGroupItem_35_1288_2").css("color", "magenta");
 //$("#ComponentGroupItem_44_1730_2").css("color", "magenta");
 
 //Custard Danish
-$("#ComponentGroupItem_45_1066_2").css("color", "magenta");
+//$("#ComponentGroupItem_45_1066_2").css("color", "magenta");
 
-//Choc Milk
-//$("#ComponentGroupItem_47_1230_2").css("color", "magenta");
+//level 400 water
+$("#ComponentGroupItem_47_1210_2").css("color", "magenta");
 
-//Thai pumpkin soup
-//$("#ComponentGroupItem_48_1641_2").css("color", "magenta");
+//low lac vanil yoghurt
+$("#ComponentGroupItem_45_1333_2").css("color", "magenta");
+
+//spinnich
+$("#ComponentGroupItem_31_950_2").css("color", "magenta");
